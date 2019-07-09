617a89e68818eb935f4c7c6033694fe45aa7db97
jhover <jhover@starkhorn.rhic.bnl.gov>
2019-07-09 13:22:29 -0400

    Update.

--- bin/restart-all.sh
+++ bin/restart-all.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -e
 service sympa stop
+service httpd stop
 service wwsympa stop
 service mariadb stop
 service postfix stop
 
 
+
 service postfix start
 service mariadb start
 service sympa start
+service httpd start
 service wwsympa start
 
